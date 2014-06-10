0de0477d86202ca1ddb47f3bd9cb555a1682e1b2
Steve (Egesté) Regester <egeste@egeste.net>
2014-06-09 19:09:19 -0700

    Update project skeleton, tasks, dependencies

--- spec/style.helper.js
+++
@@ -1,7 +0,0 @@
-var styles = ["../stylesheets/jscoverage.css"];
-require(['underscore','jquery'], function(_, $){
-  _.each(styles, function(url){
-    var style = $('<link href="' + url + '" rel="stylesheet"></link>');
-    style.prependTo('head');
-  })
-});
